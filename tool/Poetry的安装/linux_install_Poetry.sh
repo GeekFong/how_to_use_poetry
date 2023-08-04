a183ac22caf1a19addf694aa4383949fc01b8c2e
极客小方 <GeekFong@hotmail.com>
2023-08-04 09:26:30 +0800

    合并
    合并

--- tool/Poetry的安装/linux_install_Poetry.sh
+++ tool/Poetry的安装/linux_install_Poetry.sh
@@ -2,26 +2,26 @@
 
 # 检查是否已安装 Python
 if ! command -v python3 &>/dev/null; then
-    echo "未找到 Python 3,请先安装 Python 3."
+    echo "未找到 Python 3，请先安装 Python 3。"
     exit 1
 fi
 
 # 检查是否已安装 curl
 if ! command -v curl &>/dev/null; then
-    echo "未找到 curl,将尝试安装 curl."
+    echo "未找到 curl，将尝试安装 curl。"
 
     # 使用包管理器安装 curl
     if command -v apt &>/dev/null; then
         sudo apt update
         sudo apt install -y curl
-        elif command -v yum &>/dev/null; then
+    elif command -v yum &>/dev/null; then
         sudo yum install -y curl
-        elif command -v dnf &>/dev/null; then
+    elif command -v dnf &>/dev/null; then
         sudo dnf install -y curl
-        elif command -v zypper &>/dev/null; then
+    elif command -v zypper &>/dev/null; then
         sudo zypper install -y curl
-        else
-        echo "无法找到适用于当前系统的包管理器,请手动安装 curl。"
+    else
+        echo "无法找到适用于当前系统的包管理器，请手动安装 curl。"
         exit 1
     fi
 fi
@@ -38,5 +38,8 @@ if [[ ":$PATH:" != *":$home_dir/.local/bin:"* ]]; then
     source "$home_dir/.bashrc"
     echo "成功将 \$HOME/.local/bin 添加到环境变量中。"
 else
-    echo "\$HOME/.local/bin 已在环境变量中,无需添加。"
+    echo "\$HOME/.local/bin 已在环境变量中，无需添加。"
 fi
+
+#重启虚拟机
+sudo reboot

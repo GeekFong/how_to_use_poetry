a7ae1fbd83c3c21783081341cb4768cfff19fd6e
极客小方 <GeekFong@hotmail.com>
2023-08-17 09:01:42 +0800

    更新 window_install_Poetry.bat

--- tool/Poetry的安装/window_install_Poetry.bat
+++ tool/Poetry的安装/window_install_Poetry.bat
@@ -5,10 +5,10 @@ REM 安装Python Poetry
 echo Installing Python Poetry...
 powershell -Command "(Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | py -"
 
-REM 添加环境变量为%APPDATA%\Python\Scripts
-echo Adding %APPDATA%\Python\Scripts to PATH...
-set "NEW_PATH=%APPDATA%\Python\Scripts;%PATH%"
-REG ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v PATH /t REG_EXPAND_SZ /d "%NEW_PATH%" /f
+REM 添加环境变量为%%APPDATA%%\Python\Scripts
+echo Adding %%APPDATA%%\Python\Scripts to PATH...
+set "NEW_PATH=%%APPDATA%%\Python\Scripts;%%PATH%%"
+REG ADD "HKCU\Environment" /v PATH /t REG_EXPAND_SZ /d "%NEW_PATH%" /f
 
 REM 提示安装完成
 echo Installation completed successfully.

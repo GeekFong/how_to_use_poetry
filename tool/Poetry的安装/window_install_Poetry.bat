6ea45a076f74f04a6d57d4a18dffd8fcbd7db241
极客小方 <GeekFong@hotmail.com>
2023-09-20 18:03:48 +0800

    Update window_install_Poetry.bat

--- tool/Poetry的安装/window_install_Poetry.bat
+++ tool/Poetry的安装/window_install_Poetry.bat
@@ -4,7 +4,7 @@ setlocal enabledelayedexpansion
 echo Installing Python Poetry...
 powershell -Command "(Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | python -"
 
-echo Adding %%APPDATA%%\pypoetry\venv\Scripts to PATH...
+echo Adding %%APPDATA%%\Python\Scripts to PATH...
 
 REM 获取当前的系统环境变量值
 for /f "tokens=2,*" %%A in ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v PATH') do (
@@ -12,8 +12,8 @@ for /f "tokens=2,*" %%A in ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\Se
 )
 
 REM 检查是否已经包含相同的路径
-set "NEW_PATH=!CURRENT_PATH!;%APPDATA%\pypoetry\venv\Scripts"
-echo !CURRENT_PATH! | findstr /i /c:"%APPDATA%\pypoetry\venv\Scripts" > nul
+set "NEW_PATH=!CURRENT_PATH!;%APPDATA%\Python\Scripts"
+echo !CURRENT_PATH! | findstr /i /c:"%APPDATA%\Python\Scripts" > nul
 if not errorlevel 1 (
     echo Path already exists in PATH variable.
 ) else (
@@ -21,6 +21,10 @@ if not errorlevel 1 (
     REM 更新系统环境变量
     REG ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v PATH /t REG_EXPAND_SZ /d "!NEW_PATH!" /f
     echo Path added to PATH variable.
+    
+    REM 刷新当前会话的环境变量
+    refreshenv
+    echo Environment variables refreshed in the current session.
 )
 
 echo Installation completed successfully.
